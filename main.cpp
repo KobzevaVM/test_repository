62f574a12519958f3274909699c2586dbbaad5bf
violetta <kobzevavm@gmail.com>
2016-09-24 14:31:28 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int a, b;
+    cout << "введите число а" << endl;
+    cin >> a;
+    cout << "введите число b" << endl;
+    cin >> b;
+    cout << a + b << endl;
+    int c;
+    cin >> c >> endl;
+    cout << a + c << endl;
+    return 0;
+}
